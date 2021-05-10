90eac3b80c0269bf069d700d267baa2dce64a0bb
georgeryalls <gryalls@qa.com>
2021-05-10 16:22:13 +0100

    GR: Added domain package

---
+++ src/main/java/com/qa/hwa/domain/Supermarket.java
@@ -0,0 +1,69 @@
+package com.qa.hwa.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Supermarket {
+
+		@Id
+		@GeneratedValue(strategy=GenerationType.IDENTITY)
+		private Long id;
+		
+		private String name;
+		private double price;
+		private int quantity;
+		
+		public Supermarket(Long id, String name, double price, int quantity) {
+			super();
+			this.id = id;
+			this.name = name;
+			this.price = price;
+			this.quantity = quantity;
+		}
+
+		public Supermarket(String name, double price, int quantity) {
+			super();
+			this.name = name;
+			this.price = price;
+			this.quantity = quantity;
+		}
+		
+		public Supermarket() {}
+
+		public Long getId() {
+			return id;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public double getPrice() {
+			return price;
+		}
+
+		public void setPrice(double price) {
+			this.price = price;
+		}
+
+		public int getQuantity() {
+			return quantity;
+		}
+
+		public void setQuantity(int quantity) {
+			this.quantity = quantity;
+		}
+		
+		
+}
